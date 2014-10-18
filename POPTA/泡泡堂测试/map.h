eb42b09d4ce82a39afe2560f09d7b60d81b5e588
徐俊 <18800111918@163.com>
2014-10-18 13:32:59 +0800

    Move the project to the Team!

---
+++ POPTA/泡泡堂测试/map.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <hge.h>
+#include <hgefont.h>
+#include <hgegui.h>
+const int MAP_SIZE[2] = {800, 800};
+const int BOX_NUM = 20;
+const int BOX_LENGTH = 40;
+const int BOX_WIDTH = 40;
+
+
+
+class Box
+{
+public:
+	Box(void);
+	Box(int state, hgeQuad image);
+	~Box(void);
+	void state_change(int s);
+	void image_change(HGE *hge, hgeQuad image);
+	void Image_for_state(HGE *hge);
+	void Render(HGE *hge);
+	friend class map;
+private:
+	int Box_state;
+	hgeQuad Box_image;        //该方格上对应的图片
+};
+
+class map
+{
+public:
+	map(void);
+	map(HGE *hge, int state[BOX_NUM][BOX_NUM]);
+	~map(void);
+	void Render(HGE *hge);
+private:
+	Box box[BOX_NUM][BOX_NUM];
+};
+
