7cb501220c274f9b2f1973f5b5a98d66141b5151
徐俊 <18800111918@163.com>
2014-10-31 19:48:56 +0800

    嗯，我改了一下走动的代码。
    之前是模仿别人的，然后发现确实好麻烦。于是我改了一下。

--- POPTA/泡泡堂测试/popta.cpp
+++ POPTA/泡泡堂测试/popta.cpp
@@ -16,9 +16,7 @@ HTEXTURE          tex2;
 HTEXTURE          tex;
 hgeQuad           quad;
 
-person			*playerwalk;
-person			*playerstand;
-PERSON_STATE     walkorstand;
+person			*player;
 map				*Game_Map;
 	
 // Pointers to the HGE objects we will use
@@ -44,29 +42,11 @@ bool FrameFunc()
 	float fDeltaTime = hge->Timer_GetDelta();
 
 	if(dir == NO){
-		if(walkorstand == WALK){
-			playerstand->setX(playerwalk->getX());
-			playerstand->setY(playerwalk->getY());
-			playerstand->Play(playerwalk->getDirection());
-			playerwalk->Stop();
-			walkorstand = STAND;
-		}
-		else{
-			playerstand->Update(fDeltaTime);
-		}
+        player->Stop();
 	}else{
-		if(walkorstand == WALK){
-			playerwalk->setDirection(dir);
-			playerwalk->Move(SPEED, fDeltaTime);
-			playerwalk->Update(fDeltaTime);
-		}
-		else{
-			playerwalk->setX(playerstand->getX());
-			playerwalk->setY(playerstand->getY());
-			playerwalk->Play(playerstand->getDirection());
-			playerstand->Stop();
-			walkorstand = WALK;
-		}
+        player->setDirection(dir);
+        player->Move(SPEED, fDeltaTime);
+        player->Update(fDeltaTime, dir);
 	}
 
 	return false;
@@ -78,14 +58,11 @@ bool RenderFunc()
 	// Render graphics
 	hge->Gfx_BeginScene();
 	hge->Gfx_Clear(0);
-	fnt->printf(5, 5, HGETEXT_LEFT, "FPS:%d  x:%d  y:%d  walk=%d", hge->Timer_GetFPS(), playerwalk->getX(), playerwalk->getY(), int(walkorstand));
+	fnt->printf(5, 5, HGETEXT_LEFT, "FPS:%d  x:%d  y:%d", hge->Timer_GetFPS(), player->getX(), player->getY());
 	for(int i = 0; i < 20; i++)
 		for(int j = 0; j < 20; j++)
 			pic->Render(i * 40, j * 40);
-	if(walkorstand == STAND)
-		playerstand->Render();
-	else
-		playerwalk->Render();
+	player->Render();
 	hge->Gfx_EndScene();
 
 	return false;
@@ -110,16 +87,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	if(hge->System_Initiate()){
 		tex1 = hge->Texture_Load("walk.png");
-		tex2 = hge->Texture_Load("stand.png");
-		tex = hge->Texture_Load("wall1.png");	
 		pic = new hgeSprite(tex, 0, 0, BOX_WIDTH, BOX_LENGTH);
 		fnt = new hgeFont("font1.fnt");
-		playerwalk = new person(tex1, 4, 8, 100, 100);
-		playerstand = new person(tex2, 4, 8, 100, 100);
-		playerstand->setX(400);
-		playerstand->setY(400);
-		playerstand->Play(LEFT);
-		walkorstand = STAND;
+		player = new person(tex1, 4, 8, 100, 100);
+		player->setX(400);
+		player->setY(400);
+		player->Play(LEFT);
 		hge->System_Start();
 
 
